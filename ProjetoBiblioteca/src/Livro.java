3f2c9c357debaa62588544111dc79d622f98f3b9
Sofia Leiva Pires <159148454+SofiaLeivaPires@users.noreply.github.com>
2025-09-04 11:05:59 -0300

    Primeira Versão

---
+++ ProjetoBiblioteca/src/Livro.java
@@ -0,0 +1,73 @@
+public class Livro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int ano;
+    private boolean disponivel = true;
+
+    public Livro() {
+        this.disponivel = true;
+    }
+
+    public Livro(String isbn, String titulo, String autor, int ano) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+
+    }
+
+
+
+    public String getIsbn() {
+        return isbn;
+   }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "isbn='" + isbn + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", ano=" + ano +
+                ", disponivel=" + disponivel +
+                '}';
+    }
+
+}
